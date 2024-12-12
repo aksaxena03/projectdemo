99fce47b2122e44dea297354ec73532533a9f3e1
aksaxena03 <aksaxena0333@gmail.com>
2024-12-12 23:08:03 +0530

    post and get use by rect

---
+++ miscellaneous/app.js
@@ -0,0 +1,22 @@
+const express =require("express");
+const app=express();
+const port =3000;
+
+app.use(express.urlencoded({ extended: true }));
+// app.use(express.json); // it is use only when user's post data in json format
+
+app.get("/register",(req,res)=>{
+    console.log("get is listenig")
+    let {name, password}=req.query;
+    // console.log(name + password)
+
+    res.send(`hey get method ${name}`);
+});
+app.post("/register",(req,res)=>{
+    console.log("post is listenig")
+    // console.log(req.body)
+    let {name,pass}=req.body;
+    res.send(`hey post method ${name} `)
+});
+
+app.listen(port)
